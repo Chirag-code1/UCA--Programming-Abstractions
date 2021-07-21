f64a22c29f4188c60e18610ee485681b9b410c7c
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-07-21 14:25:24 +0530

    Create DecimalIntToBinary.cpp

---
+++ DecimalIntToBinary.cpp
@@ -0,0 +1,25 @@
+
+//DECIMAL INT TO BINARY:
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+using namespace std;
+
+void dtb (int n){
+//quotient = n/2;
+        int rem=0, ans=0, i=1;
+    while(n>0){
+        rem = n%2;
+        ans = ans + rem*i;
+        i*=10;
+        n/=2;
+    }
+    cout<<"Answer:\t"<<ans;
+}
+
+int main()
+{
+    int n = 43;
+    dtb(n);
+   return 0;
+}
