18c818efc9e105c15181ef096a16b411cc49b499
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-07-26 22:05:53 +0530

    Create Pattern2.cpp
    Write a program to print the n lines of the below pyramid
    
    1
    
    212
    
    32123
    
    4321234
    
    543212345

---
+++ Pattern2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+using namespace std;
+int main()
+{	
+  int n;
+  cin>>n;
+  
+  int i, j, k;
+    for(i=1;i<=n;i++){ //represents row number.
+
+        //to print reverse. 
+        for(j=i;j>=1;j--){
+            cout<<j<<"";
+        }
+        if(i==1){
+            cout<<"\n";
+        }
+
+        //to print number is forward order from 2, from 2nd row.
+        if(i>=2){
+            for(k=2;k<=i;k++){
+                if(k == i)
+               { cout<<k<<endl; }
+                else{ cout<<k<<"";}
+
+            }
+        }
+        
+    }
+  
+  
+  
+   return 0;
+}
