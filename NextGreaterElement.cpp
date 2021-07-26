cec73c6217fd434228cb3426ec620c497a3489c4
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-07-26 15:52:52 +0530

    Array updation based on Next Greater Number
    Given an array having distinct integer elements, the task is to find the first next greater element on right side for each element of the array. If no such element exists, output -1.
    BUT, 
    Update the given array in such a way that the indices where the present element if is smaller than element in right, should contain -1 and greater number if it exists on right.

---
+++ NextGreaterElement.cpp
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+using namespace std;
+
+void greaterElement(int arr[], int n){
+    for(int i=1;i<=n;i++){
+        if(arr[i]<=arr[i+1] && i!=n){
+            arr[i] = arr[i+1];
+           // cout<<arr[i+1]<<"   ";
+        }
+        else if(arr[i] == arr[n] && i==n)
+           { arr[i] = -1; }
+            //cout<<"-1"<<endl; 
+        else{
+            arr[i] = -1;
+        }
+        
+    }
+    return arr;
+
+    cout<<"Final Array:\t"<<endl;
+    for(int i=1;i<=n;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=1;i<=n;i++){
+        cin>>arr[i];
+    }
+    greaterElement(arr,n);
+   return 0;
+}
