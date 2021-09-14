37efb1e49a68d486e83d41d90ca39220a3ff5c11
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-09-15 00:37:19 +0530

    Create MajorityElement.cpp
    Given an array of size n, find the majority element. The majority element is the element that appears more than n/2 times.
    
    You may assume that the array is non-empty and and the contents of array are always positive
    
    Complete the function given getMajorityElement() in the editor, that accepts an array & returns the majority element of the array.

---
+++ MajorityElement.cpp
@@ -0,0 +1,20 @@
+void getMajorityElement(int array[], int size) {
+    int majorityElement=-1, flag=1;
+    int reqTimes = size/2;
+    for(int i=0; i<size-1; i++){
+        flag =1;
+        for(int j=i+1; j<size; j++){
+            if(array[i] == array[j]){
+                flag++;
+            }
+        }
+
+        if(flag > reqTimes){
+            cout<<"Flag at "<<i<<" index element is: "<<flag<<endl;
+            majorityElement = array[i];
+        }
+                    
+    }
+    cout<<"Majority Element is: "<<majorityElement<<endl;
+
+}
