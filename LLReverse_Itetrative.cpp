93bf5d02ec8ff6f16943dee370ba496b301e9bcc
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-09-15 22:59:12 +0530

    Create LLReverse_Itetrative.cpp
    A linked list is a collection of nodes. First node is called head node and last node called the tail node.
    
    Now, given a pointer to the head node of a linked list, can you reverse the linked list i.e. New list must start with tail node and end at head node. So it becomes the reverse of the original list.
    
    Complete the function reverseList() which takes the head node of a linked list as a parameter and return the new head of reversed list.

---
+++ LLReverse_Itetrative.cpp
@@ -0,0 +1,15 @@
+struct Node* reverseList(struct Node* head) {
+  // Itetrative:
+  	Node* preptr=NULL;
+   Node* curptr=head;
+   Node* nextptr;
+   while(curptr!=NULL){
+       nextptr=curptr->next;
+       curptr->next=preptr;
+        
+       preptr=curptr;
+       curptr=nextptr;
+   }
+   return preptr;
+ 
+}
